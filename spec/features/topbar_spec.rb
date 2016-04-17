e45f4c9bc42bfaf755f8083955acc7d340c85f65
SriKalai <sriganeshkalai@gmail.com>
2016-04-17 21:06:00 +0100

    finish topbar spec

--- spec/features/topbar_spec.rb
+++ spec/features/topbar_spec.rb
@@ -3,6 +3,16 @@ require 'rails_helper'
 describe "top bar" do
   before(:each) {visit root_path}
 
+  it 'has the necessary div styles' do
+    expect(page).to have_css("section.top_sec")
+    expect(page).to have_css("div.container")
+    expect(page).to have_css("div.col-xs-12.col-md-6.top_lft")
+    expect(page).to have_css("div.soc_ico")
+    expect(page).to have_css("div.inf_txt")
+    expect(page).to have_css("i.fa-phone-square")
+    expect(page).to have_css("div.submit_prop")
+  end
+
   it 'display Shri Maya travels caption' do
     expect(page).to have_content('Shri Maya Travels - Explore Your Sprituality')
   end
@@ -11,4 +21,9 @@ describe "top bar" do
     expect(page).to have_content('Call Our Travel Experts')
     expect(page).to have_content('+91 7500113007 / 9897874915')
   end
+
+  it 'display the free callback link' do
+    expect(page).to have_css("a", :text => "Free Callback")
+  end
+
 end
